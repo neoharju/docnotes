a6313ab2617bd9edd1ddbdd82d54ca4a40db0a0b
neoharju <38046336+neoharju@users.noreply.github.com>
2026-09-04 18:28:21 +0300

    feat(config): basic confs

---
+++ config.py
@@ -0,0 +1,31 @@
+"""App configuration"""
+
+
+class Config:
+    """App confs"""
+
+    SECRET_KEY = "123"  # os.environ["SECRET_KEY"]
+    DATABASE = "database.db"
+
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPOnly = True
+    SESSION_COOKIE = SAMESITE = "Lax"
+
+    # NOTE: max upload 32 MiB; unused still
+    MAX_CONTENT_LENGTH = 32 * 1024 * 1024
+
+
+class DevConfig(Config):
+    """Development configurations"""
+
+    DEBUG = True
+    TESTING = False
+
+
+class TestConfig(Config):
+    """Test configurations"""
+
+    TESTING = True
+    DEBUG = False
+
+    DATABASE = "test_database.db"
